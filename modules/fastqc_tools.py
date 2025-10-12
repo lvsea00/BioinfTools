d6fdb2067d5ea78bb58b6e1b4d35724f79dca367
lvsea00 <lvsea00@gmail.com>
2025-10-12 16:09:05 +0300

    Redesigne read quality checking function

--- modules/fastqc_tools.py
+++ modules/fastqc_tools.py
@@ -6,10 +6,12 @@ def check_gc_count(seq: str, gc_lower: float, gc_upper: float) -> bool:
     return gc_lower <= gc_content <= gc_upper
 
 
-def mean_quality(seq: str) -> float:
-    """Counts mean quality of the read"""
+def check_quality(seq: str, quality_threshold: int) -> bool:
+    """
+    Checks if quality of the read matches given conditions
+    """
     quality_sum: int = 0
     for char in seq:
         quality_sum += ord(char) - 33
     mean_quality = quality_sum/len(seq)
-    return mean_quality
+    return mean_quality > quality_threshold
