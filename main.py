54469c96424c42058011a763d1793294e9625963
lvsea00 <lvsea00@gmail.com>
2026-03-01 21:11:10 +0300

    add types annotations

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
+import os
+from typing import Union
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
 from abc import ABC, abstractmethod
 
+
 class BiologicalSequence(ABC):
-    def __init__(self, seq):
+    def __init__(self, seq: str) -> None:
         self.seq = seq
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.seq)
-    
-    def __getitem__(self, index):
+
+    def __getitem__(self, index: int) -> str:
         return self.seq[index]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.__class__.__name__}: {self.seq}"
-    
+
     @abstractmethod
-    def validate(self):
+    def validate(self) -> bool:
         pass
 
 
 class NucleicAcidSequence(BiologicalSequence):
     complement_dict = None
 
-    def __init__(self, seq):
+    def __init__(self, seq: str):
         super().__init__(seq)
         if not self.validate():
-            raise ValueError(f"Invalid sequence")
-    
-    def validate(self):
+            raise ValueError("Invalid sequence")
+
+    def validate(self) -> bool:
         for char in self.seq:
             if char not in self.alphabet:
                 return False
         return True
 
-    def reverse(self):
+    def reverse(self) -> 'NucleicAcidSequence':
         """Returns reverse sequence"""
         return self.__class__(self.seq[::-1])
 
-    def complement(self):
+    def complement(self) -> 'NucleicAcidSequence':
         """Returns complement sequence"""
         if self.complement_dict is None:
             raise NotImplementedError
         complement_seq = ''.join(self.complement_dict[char] for char in self.seq)
         return self.__class__(complement_seq)
 
-    def reverse_complement(self):
+    def reverse_complement(self) -> 'NucleicAcidSequence':
         """Returns reverse complement sequence"""
         return self.reverse().complement()
 
@@ -51,8 +56,8 @@ class NucleicAcidSequence(BiologicalSequence):
 class DNASequence(NucleicAcidSequence):
     alphabet = set('ATGC')
     complement_dict = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
-    
-    def transcribe(self):
+
+    def transcribe(self) -> 'RNASequence':
         """Returns RNA sequence transcribed from DNA"""
         transcribed_seq = self.seq.replace('T', 'U')
         return RNASequence(transcribed_seq)
@@ -65,28 +70,21 @@ class RNASequence(NucleicAcidSequence):
 
 class AminoAcidSequence(BiologicalSequence):
     alphabet = {'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
-    'L', 'K', 'M', 'F', 'P', 'O', 'S', 'U', 'T', 'W', 'Y', 'V'}
+                'L', 'K', 'M', 'F', 'P', 'O', 'S', 'U', 'T', 'W', 'Y', 'V'}
 
     def __init__(self, seq):
         super().__init__(seq)
         if not self.validate():
-            raise ValueError(f"Invalid sequence")
-    
-    def validate(self):
+            raise ValueError("Invalid sequence")
+
+    def validate(self) -> bool:
         for char in self.seq:
             if char not in self.alphabet:
                 return False
-        return True           
-    
-    def met_start(self):
+        return True
+
+    def met_start(self) -> bool:
         return self.seq[0].upper() == 'M'
-        
-
-
-import os
-from typing import Union
-from Bio import SeqIO
-from Bio.SeqUtils import gc_fraction
 
 
 def filter_fastq(input_fastq: str,
@@ -108,7 +106,7 @@ def filter_fastq(input_fastq: str,
         str: Program work result information.
 
     """
-    
+
     gc_lower, gc_upper = gc_bounds if isinstance(gc_bounds, tuple) else (0, gc_bounds)
     len_min, len_max = length_bounds if isinstance(length_bounds, tuple) else (0, length_bounds)
 
@@ -129,9 +127,3 @@ def filter_fastq(input_fastq: str,
                 SeqIO.write(record, output_file, "fastq")
 
     print("Sequences are filtered out")
-                
-                      
-
-
-
-
