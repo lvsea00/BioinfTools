0cbb1788bd611213199b693d30dcec1c40c31a62
lvsea00 <lvsea00@gmail.com>
2026-03-01 20:44:51 +0300

    make filter_fastq function

--- main.py
+++ main.py
@@ -83,103 +83,10 @@ class AminoAcidSequence(BiologicalSequence):
         
 
 
-dna = DNASequence("ATGC")
-rna = RNASequence("AUGC")
-protein = AminoAcidSequence("MVLSPAD")
-
-# Проверяем методы
-print(dna)                          # DNASequence: ATGC
-print(dna.complement())              # DNASequence: TACG
-print(dna.reverse())                 # DNASequence: CGTA  
-print(dna.reverse_complement())      # DNASequence: GCAT
-
-print(dna.transcribe())              # RNASequence: AUGC
-
-print(rna.complement())              # RNASequence: UACG
-
-print(protein)                       # AminoAcidSequence: MVLSPAD
-print(protein.met_start()) 
-
-
-
 import os
-
-
-def check_gc_count(seq: str, gc_lower: float, gc_upper: float) -> bool:
-    """
-    Checks if GC percentage of the read matches given conditions
-    """
-    gc_content = (seq.count('G') + seq.count('C')) * 100 / len(seq)
-    return gc_lower <= gc_content <= gc_upper
-
-
-def check_len(seq: str, len_min: int, len_max: int) -> bool:
-    """
-    Checks if length of the read matches given conditions
-    """
-    return len_min <= len(seq) <= len_max
-
-
-def check_quality(seq: str, quality_threshold: int) -> bool:
-    """
-    Checks if quality of the read matches given conditions
-    """
-    quality_sum: int = 0
-    for char in seq:
-        quality_sum += ord(char) - 33
-    mean_quality = quality_sum/len(seq)
-    return mean_quality > quality_threshold
-
-
-def fastq_to_dict(input_fastq: str) -> dict:
-    """
-    Converts fastq file to dictionary.
-
-    Args:
-        input_fastq (str): Absolute path to fastq file.
-
-    Returns:
-        dict: Dictionary with fastq sequences.
-    """
-    fastq_seqs = dict()
-    with open(input_fastq) as fastq_file:
-        for line in fastq_file:
-            if line.startswith('@'):
-                seq_id = line.strip()
-                seq = fastq_file.readline().strip()
-                fastq_file.readline().strip()
-                quality = fastq_file.readline().strip()
-            fastq_seqs[seq_id] = (seq, quality)
-    return fastq_seqs
-
-
-def save_filtered(filtered_seqs: dict, output_fastq: str):
-    """
-    Save filtered fastq sequences from dictionary to the file.
-
-    Args:
-        filtered_seqs (dict): Dictionary with filtered fastq sequences
-        output_fastq (str): Name of fastq file with filtered sequences.
-
-    Returns:
-        Program work result information.
-    """
-    if 'filtered' not in os.listdir():
-        os.mkdir('filtered')
-    filtered_path = os.path.join(os.getcwd(), 'filtered')
-    if not os.path.isfile(os.path.join(filtered_path, output_fastq)):
-        with open(os.path.join(filtered_path, output_fastq), mode='w') as fastq_file:
-            for name, (seq, quality) in filtered_seqs.items():
-                fastq_file.write(name + '\n' + seq + '\n' + '+' + name[1:] + '\n' + quality + '\n')
-        print("Sequences are filtered out")
-    else:
-        print("Check the name of the output file! Risk of overwriting!")
-
-
-
-
-
 from typing import Union
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction
 
 
 def filter_fastq(input_fastq: str,
@@ -191,7 +98,7 @@ def filter_fastq(input_fastq: str,
     Filters reads based on QC.
 
     Args:
-        input_fastq (str): Absolute path to fastq file with unfiltered sequences.
+        input_fastq (str): Path to fastq file with unfiltered sequences.
         output_fastq (str): Name of fastq file with filtered sequences.
         gc_bounds (tuple, float): GC percentage bounds.
         length_bounds (tuple, int): Read length bounds.
@@ -201,11 +108,30 @@ def filter_fastq(input_fastq: str,
         str: Program work result information.
 
     """
-    fastq_seqs = fastq_to_dict(input_fastq)
-    filtered_seqs = dict()
+    
     gc_lower, gc_upper = gc_bounds if isinstance(gc_bounds, tuple) else (0, gc_bounds)
     len_min, len_max = length_bounds if isinstance(length_bounds, tuple) else (0, length_bounds)
-    for name, (seq, quality) in fastq_seqs.items():
-        if check_gc_count(seq, gc_lower, gc_upper) and check_len(seq, len_min, len_max) and check_quality(quality, quality_threshold):
-            filtered_seqs[name] = (seq, quality)
-    return save_filtered(filtered_seqs, output_fastq)
+
+    output_dir = 'filtered'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_path = os.path.join(output_dir, output_fastq)
+
+    with open(input_fastq, 'r') as input_file, open(output_path, mode='w') as output_file:
+        records = SeqIO.parse(input_file, "fastq")
+        for record in records:
+            seq = record.seq
+            gc_content = gc_fraction(seq) * 100
+            qualities = record.letter_annotations["phred_quality"]
+            mean_quality = sum(qualities) / len(qualities)
+
+            if (len_min <= len(seq) <= len_max) and (gc_lower <= gc_content <= gc_upper) and (mean_quality > quality_threshold):
+                SeqIO.write(record, output_file, "fastq")
+
+    print("Sequences are filtered out")
+                
+                      
+
+
+
+
